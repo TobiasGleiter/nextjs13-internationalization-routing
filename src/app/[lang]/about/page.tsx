2524a43cd94dc0518b262a2caef8237d79ceb8fc
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-11-21 08:44:23 +0100

    refactor: about page

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,20 +1,30 @@
+import { Locale } from '@/src/i18n.config';
+import { getDictionary } from '@/src/lib/lang';
 import { Metadata, ResolvingMetadata } from 'next';
 
 type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
+  params: { lang: Locale };
 };
 
 export async function generateMetadata(
-  { params, searchParams }: Props,
+  { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
+  const { meta } = await getDictionary(params.lang);
+
   return {
-    title: 'Hallo',
-    description: 'Description',
+    title: meta.title,
+    description: meta.description,
   };
 }
 
-export default function Page({ params, searchParams }: Props) {
-  return <div>AboutPage</div>;
+export default async function Page({ params }: Props) {
+  const { page } = await getDictionary(params.lang);
+
+  return (
+    <div>
+      <h1>{page.about.title}</h1>
+      <p>{page.about.description}</p>
+    </div>
+  );
 }
